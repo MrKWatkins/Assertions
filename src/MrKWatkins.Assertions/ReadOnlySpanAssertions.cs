78dcd0d7a8581195172d8f0350d6122cbd70f208
MrKWatkins <mrkwatkins@gmail.com>
2025-01-21 19:22:09 +0000

    Move SequenceEqual for ReadOnlySpans onto ReadOnlySpanAssertions.

--- src/MrKWatkins.Assertions/ReadOnlySpanAssertions.cs
+++ src/MrKWatkins.Assertions/ReadOnlySpanAssertions.cs
@@ -22,4 +22,81 @@ public readonly ref struct ReadOnlySpanAssertions<T>
 
         return new ReadOnlySpanAssertionsChain<T>(this);
     }
+
+    public ReadOnlySpanAssertionsChain<T> SequenceEqual(params ReadOnlySpan<T> expected)
+    {
+        var equalityComparer = EqualityComparer<T>.Default;
+        if (Value.Length != expected.Length)
+        {
+            throw Verify.CreateException(
+                $"Value {Format.Value(Value)} should sequence equal {Format.Value(expected)} but it has {Value.Length} element{(Value.Length == 1 ? "" : "s")} rather than the expected {expected.Length}.");
+        }
+
+        for (var f = 0; f < Value.Length; f++)
+        {
+            var actualItem = Value[f];
+            var expectedItem = expected[f];
+            if (!equalityComparer.Equals(actualItem, expectedItem))
+            {
+                throw Verify.CreateException(
+                    $"Value {Format.Value(Value, f)} should sequence equal {Format.Value(expected, f)} but it differs at index {f}.");
+            }
+        }
+
+        return new ReadOnlySpanAssertionsChain<T>(this);
+    }
+
+    public ReadOnlySpanAssertionsChain<T> NotSequenceEqual(params ReadOnlySpan<T> expected)
+    {
+        if (Value.SequenceEqual(expected))
+        {
+            throw Verify.CreateException($"Value should not sequence equal {Format.Value(expected)}.");
+        }
+
+        return new ReadOnlySpanAssertionsChain<T>(this);
+    }
+
+    public ReadOnlySpanAssertionsChain<T> SequenceEqual(IReadOnlyList<T> expected)
+    {
+        var equalityComparer = EqualityComparer<T>.Default;
+        if (Value.Length != expected.Count)
+        {
+            throw Verify.CreateException(
+                $"Value {Format.Value(Value)} should sequence equal {Format.Value(expected)} but it has {Value.Length} element{(Value.Length == 1 ? "" : "s")} rather than the expected {expected.Count}.");
+        }
+
+        for (var f = 0; f < Value.Length; f++)
+        {
+            var actualItem = Value[f];
+            var expectedItem = expected[f];
+            if (!equalityComparer.Equals(actualItem, expectedItem))
+            {
+                throw Verify.CreateException(
+                    $"Value {Format.Value(Value, f)} should sequence equal {Format.Value(expected, f)} but it differs at index {f}.");
+            }
+        }
+
+        return new ReadOnlySpanAssertionsChain<T>(this);
+    }
+
+    public ReadOnlySpanAssertionsChain<T> NotSequenceEqual(IReadOnlyList<T> expected)
+    {
+        var equalityComparer = EqualityComparer<T>.Default;
+        if (Value.Length != expected.Count)
+        {
+            return new ReadOnlySpanAssertionsChain<T>(this);
+        }
+
+        for (var f = 0; f < Value.Length; f++)
+        {
+            var actualItem = Value[f];
+            var expectedItem = expected[f];
+            if (!equalityComparer.Equals(actualItem, expectedItem))
+            {
+                return new ReadOnlySpanAssertionsChain<T>(this);
+            }
+        }
+
+        throw Verify.CreateException($"Value should not sequence equal {Format.Value(expected)}.");
+    }
 }
